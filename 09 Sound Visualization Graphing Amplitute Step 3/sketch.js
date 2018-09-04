742f94b4f5813c7a7e9c707c974f707346953207
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-09-04 19:48:58 -0400

    Update sketch.js

--- 09 Sound Visualization Graphing Amplitute Step 3/sketch.js
+++ 09 Sound Visualization Graphing Amplitute Step 3/sketch.js
@@ -1,41 +1,37 @@
-//get rid of the micphone in this part
-
 var song;
 var amp;
-
 var historys = [];
 
-function preload() {
-	song = loadSound("background.mp3");
-}
-
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 500);
+    song = loadSound("background.mp3", loaded);
     colorMode(HSB);
-    song.play();
+
     amp = new p5.Amplitude();
 }
 
+
 function draw() {
     background(0);
-    noFill();
-    stroke(frameCount % 255, 255, 255);
-    var level = amp.getLevel();
+
+    var level = amp.getLevel();//!!!!!!return value is pritty small
     historys.push(level);
 
-    var currentY = map(level, 0, 1, height, 0);
-    translate(0, height / 2 - currentY);
+    stroke(frameCount % 255, 255, 255);
+    noFill();
+    
+    var y = map(level, 0, 1, height, 0);
+    translate(0, height / 2 - y);
 
     push();
-    beginShape();
-        for(let i = 0; i < historys.length; i++) {
-            var y = map(historys[i], 0, 1, height, 0);
-            vertex(i, y);
-        }
-    endShape();
+        beginShape();
+            for(let i = 0; i < historys.length; i++) {
+                var y = map(historys[i], 0, 1, height, 0);
+                vertex(i, y);
+            }
+        endShape();
     pop();
 
-    stroke(255);
     line(historys.length, 0, historys.length, height);
 
     if(historys.length > width - 50) {
@@ -44,10 +40,7 @@ function draw() {
 }
 
 
-
-
-
-
-
-
-
+function loaded() {
+    console.log("Loaded");
+    song.play();
+}

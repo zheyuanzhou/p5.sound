f8e14c8a86804f8112467e6978cf7b4575681329
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-09-04 19:07:01 -0400

    Update sketch.js

--- 09 Sound Visualization Graphing Amplitute Step 1/sketch.js
+++ 09 Sound Visualization Graphing Amplitute Step 1/sketch.js
@@ -4,19 +4,20 @@ var song;
 var amp;
 
 function preload() {
-	song = loadSound("background.mp3");
+    song = loadSound("background.mp3");
 }
 
 function setup() {
-    createCanvas(400, 400);
-    song.play();
+    createCanvas(500, 500);
+    song = loadSound("background.mp3", loaded);
+
     amp = new p5.Amplitude();
 }
 
 function draw() {
     background(0);
 
-    var level = amp.getLevel();
+    var level = amp.getLevel();//!!!!!!return value is pritty small
     // var size = map(level, 0, 1, 0, 200);
 
     fill(200, 0, 100);

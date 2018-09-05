7888683f05b9c66031310b8ddaad79f6d8bad5ee
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-09-05 13:04:06 -0400

    Update sketch.js

--- 10 Sound Visualization Radial Graph/sketch.js
+++ 10 Sound Visualization Radial Graph/sketch.js
@@ -7,36 +7,38 @@ function setup() {
     angleMode(DEGREES);
     colorMode(HSB);
     song = loadSound("background.mp3", loaded);
-    amp = new p5.Amplitude();//just create new amp object by amp constructor
-}
 
-function loaded() {
-	console.log("loaded");
-	song.play();
+    amp = new p5.Amplitude();
 }
 
 function draw() {
     background(0);
+    translate(width / 2, height / 2);
     
+    var vol = amp.getLevel();
+    historys.push(vol);
+    // var size = map(vol, 0, 1, 100, 300);
+
+    // fill(200, 0, 100);
+    // ellipse(width / 2, height / 2, size, size);
     stroke(frameCount % 255, 255, 255);
     noFill();
-
-    var vol = amp.getLevel();//use getLEvel to get the range of volume
-    historys.push(vol);//put all volmume into one empty array
-
-    translate(width / 2, height / 2);
     beginShape();
-    	for(let angle = 0; angle < 360; angle++) {
-    		// var y = map(historys[i], 0, 1, height / 2, 0);
-    		var r = map(historys[angle], 0, 1, 180, 500);
-    		var x = r * cos(angle);
-    		var y = r * sin(angle);
-    		vertex(x, y);
-    	}
+        for(let angle = 0; angle < 360; angle++) {
+            var r = map(historys[angle], 0, 1, 100, 500);
+            var x = r * cos(angle);
+            var y = r * sin(angle);
+            vertex(x, y);
+        }
     endShape();
 
     if(historys.length > 360) {
-    	historys.splice(0, 1);
+        historys.splice(0, 1);
     }
 
+}
+
+function loaded() {
+    console.log("Loaded");
+    song.play();
 }
